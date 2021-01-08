d77c7dd5c3f442e085c847f14dcda971d7b00e80
harshcut <66206863+harshcut@users.noreply.github.com>
2021-01-08 16:27:48 +0530

    feat: add @components/Layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,5 @@ export { default as Alert } from "./Alert";
 export { default as CheckBox } from "./CheckBox";
 export { default as PrivateRoute } from "./PrivateRoute";
 export { default as Loading } from "./Loading";
+export { default as Header } from "./Layout/Header";
+export { default as Footer } from "./Layout/Footer";
