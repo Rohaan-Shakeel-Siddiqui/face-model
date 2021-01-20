fb528a70495c675e015e1b5cdbf0090d0fffbca3
harshcut <66206863+harshcut@users.noreply.github.com>
2021-01-20 15:39:47 +0530

    feat: visualize clarifai response

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,17 @@
-import { useState, useRef, Fragment } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { useSelector } from "react-redux";
-import { Input, Button, Alert } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
+import { Input, Button, Alert, Loading } from "../../components";
 import { alertSnippets } from "../../utils";
 import Anchor from "../../components/Layout/Anchor";
+import { login, logout } from "../../actions";
+import { getImageDetection } from "../../api";
 
 const Dashboard = () => {
   const { register, handleSubmit } = useForm();
-  const data = useSelector(
+  const dispatch = useDispatch();
+  const user = useSelector(
     (state: {
       user: {
         data: {
@@ -21,32 +24,113 @@ const Dashboard = () => {
       };
     }) => state.user.data
   );
+  const [badImage, setBadImage] = useState(false);
+  const [image, setImage] = useState("");
+  const [faceCount, setFaceCount] = useState(0);
+  const [validity, setValidity] = useState(true);
   const [loading, setLoading] = useState(false);
-  const [badImage] = useState(false);
-  const [image] = useState("");
-  const [boundingBoxes] = useState([]);
-  const [validity] = useState(true);
-  const outputImage = useRef(null);
+  const outputImage = useRef<HTMLImageElement>(null);
+  const [boundingBoxes, setBoundingBoxes] = useState<Array<React.ReactNode>>(
+    []
+  );
 
-  const onSubmit = (data: { image: string }) => {
-    console.log(data.image);
-    setLoading(true);
-  };
+  const getBoundingBox = useCallback(
+    (clarifai: { outputs: [{ data: { regions: [] } }] }) => {
+      try {
+        const data = clarifai.outputs[0].data.regions;
+        let stash: Array<React.ReactNode> = [];
+        data.forEach(
+          (
+            data: {
+              region_info: {
+                bounding_box: {
+                  left_col: number;
+                  top_row: number;
+                  right_col: number;
+                  bottom_row: number;
+                };
+              };
+            },
+            index: number
+          ) => {
+            const width = Number(outputImage.current!.width);
+            const height = Number(outputImage.current!.height);
+            stash.push(
+              <div
+                key={index}
+                className="bounding-box"
+                style={{
+                  left: data.region_info.bounding_box.left_col * width,
+                  top: data.region_info.bounding_box.top_row * height,
+                  right:
+                    width - data.region_info.bounding_box.right_col * width,
+                  bottom:
+                    height - data.region_info.bounding_box.bottom_row * height,
+                }}
+              />
+            );
+          }
+        );
+        setBoundingBoxes(stash);
+        setFaceCount(clarifai.outputs[0].data.regions.length);
+      } catch {
+        setBoundingBoxes([]);
+        setFaceCount(0);
+      }
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (user.entries >= 21) {
+      setValidity(false);
+    }
+    if (outputImage.current && image) {
+      outputImage.current.onload = () => {
+        setBadImage(false);
+        setLoading(true);
+        setBoundingBoxes([]);
+        setFaceCount(0);
+        getImageDetection(image)
+          .then(({ response, status }) => {
+            if (status === 200) {
+              if (response.entries <= 21) {
+                dispatch(login({ ...user, entries: response.entries }));
+                getBoundingBox(response.clarifai);
+              } else {
+                setValidity(false);
+              }
+            } else {
+              setBadImage(true);
+              setLoading(false);
+              setBoundingBoxes([]);
+              setFaceCount(0);
+            }
+          })
+          .catch(() => dispatch(logout()))
+          .finally(() => setLoading(false));
+      };
+    }
+  }, [dispatch, getBoundingBox, image, user]);
 
   return (
     <div className="pt-6 px-5 pb-6 w-full sm:max-w-xl grid gap-3">
       <div className="flex justify-between">
         <span>
-          {data.first_name.toUpperCase() + " " + data.last_name.toUpperCase()}
+          {user.first_name.toUpperCase() + " " + user.last_name.toUpperCase()}
         </span>
-        <Link to="/" className="text-custom-200">
+        <Link
+          to="/"
+          className="text-custom-200"
+          onClick={() => dispatch(logout())}
+        >
           LOGOUT
         </Link>
       </div>
       {validity ? (
-        <Fragment>
+        <React.Fragment>
           <form
-            onSubmit={handleSubmit(onSubmit)}
+            onSubmit={handleSubmit((data) => setImage(data.image))}
             className="text-base grid sm:flex gap-3"
             noValidate
           >
@@ -59,35 +143,41 @@ const Dashboard = () => {
               />
             </div>
             <div className="sm:w-32">
-              <Button loading={loading}>DETECT</Button>
+              <Button>DETECT</Button>
             </div>
           </form>
           <Alert>
             <div className="grid gap-3">
               <div className="flex justify-between">
                 <span>STATUS: ACTIVE</span>
-                <span>{data.entries}/20</span>
+                <span>{user.entries}/20</span>
               </div>
               <hr style={{ color: "#4dbb5fd3" }} />
               <p>
-                {alertSnippets.demo} Click <u>here</u> for demo.
+                {alertSnippets.demo} Click{" "}
+                <span
+                  onClick={() =>
+                    setImage("https://www.atcofficial.com/images/headerBg.jpg")
+                  }
+                  className="border-b cursor-pointer"
+                >
+                  here
+                </span>{" "}
+                for demo.
               </p>
             </div>
           </Alert>
-        </Fragment>
+        </React.Fragment>
       ) : (
-        <Alert>{alertSnippets.exceeded}</Alert>
+        <Alert type="warning">{alertSnippets.exceeded}</Alert>
       )}
       {badImage && <Alert type="warning">{alertSnippets.image}</Alert>}
+      {loading && <Loading type="linear" />}
       <div className="rounded-md relative">
-        <img
-          ref={outputImage}
-          src={image}
-          alt=""
-          className="content-image rounded"
-        />
+        <img ref={outputImage} src={image} alt="" className="rounded w-full" />
         {boundingBoxes}
       </div>
+      {image && <div>Total Face Count {faceCount}</div>}
       <Anchor />
     </div>
   );
